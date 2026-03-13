2f6909a9372b5c8a8c2c036929cafe644f404f68
Pham Tan <phamtanbetamn@gmail.com>
2026-03-14 01:01:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(cors());
 app.use("/api/contacts", contactsRouter)
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
